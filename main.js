e9f59c2a868a6e6a1fe11fb8c08778dde4dd041b
Takashi Maeda <jjs-trainer@outlook.jp>
2014-05-20 17:26:14 +0900

    descriptionというid名が適切じゃなかったので修正

--- main.js
+++ main.js
@@ -3,35 +3,35 @@ window.onload = function() {
     'use strict';
 
     var rand = Math.floor(Math.random() * 5 + 1),
-        result, description;
+        result, message;
 
     switch(rand) {
     case 1:
         result = "大吉";
-        description = "いいわー、すごいいいいわー。";
+        message = "いいわー、すごいいいいわー。";
         break;
 
     case 2:
         result = "中吉";
-        description = "普通だねー。";
+        message = "普通だねー。";
         break;
 
     case 3:
         result = "小吉";
-        description = "んー、ちょっとあれだねー。";
+        message = "んー、ちょっとあれだねー。";
         break;
 
     case 4:
         result = "凶";
-        description = "よくないねー。";
+        message = "よくないねー。";
         break;
 
     case 5:
         result = "大凶";
-        description = "最悪だねー。";
+        message = "最悪だねー。";
         break;
     }
 
     document.getElementById("result").innerText = result;
-    document.getElementById("description").innerText = description;
+    document.getElementById("message").innerText = message;
 }
